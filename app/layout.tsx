d373d290b27971ea402335ba958592478bef2894
unknown <Admin@INIVOS-LAP85.braveenergy.local>
2026-09-09 23:59:43 +0530

    Add wedding preview and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,38 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://sandundilini.vercel.app"),
+
   title: "Sandun & Dilini",
-  description: "Find your table at the wedding of Sandun & Dilini",
+
+  description:
+    "Welcome to our Home coming • Find your table number",
+
+  openGraph: {
+    title: "Sandun & Dilini",
+    description:
+      "Welcome to our Home coming • Find your table number",
+    url: "https://sandundilini.vercel.app",
+    siteName: "Sandun & Dilini",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Sandun & Dilini Wedding",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Sandun & Dilini",
+    description:
+      "Welcome to our Home coming • Find your table number",
+    images: ["/opengraph-image.png"],
+  },
 };
 
 export default function RootLayout({
